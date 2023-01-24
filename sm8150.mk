b0e486569dc3a55ade6ac0c9705ebcbc28780118
Xpsoted <37337416+Xpsoted@users.noreply.github.com>
2023-01-24 10:07:35 +0800

    sm8150_common:fix no vibrator on PixelExperience

--- sm8150.mk
+++ sm8150.mk
@@ -440,7 +440,10 @@ PRODUCT_PACKAGES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    android.hardware.vibrator@1.3-service.lge
+      vendor.qti.hardware.vibrator.service
+
+PRODUCT_COPY_FILES += \
+      vendor/qcom/opensource/vibrator/excluded-input-devices.xml:$(TARGET_COPY_OUT_VENDOR)/etc/excluded-input-devices.xml
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
